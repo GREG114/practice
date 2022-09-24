a365f5200cf4d87fc2ef631a47e5ad4fd2d99db7
liuchao <greg_114@163.com>
2022-09-24 17:37:32 +0800

    update20220924

--- greg_pkg_release/elkhelper.py
+++ greg_pkg_release/elkhelper.py
@@ -1,127 +1,77 @@
 import datetime
 from dateutil.parser import parse
 import requests
+import pytz
 import json
 
 class elkhelper():
     '''
     elk操作类
     整体不做异常处理,请在使用时单独处理
+
+
+    20220509研究根据条件更新数据
+    get sh-market-seo/_search
+    {
+    "query":{
+        "bool":{
+        "must_not":{
+            "exists":{
+            "field":"排名"
+            }
+        }
+        }  
+    }
+    }
+
+
+    post sh-market-seo/_update_by_query
+    {   
+    "script": {
+            "source": "ctx._source['排名']=999"
+        },
+    "query":{
+        "match":{
+        "_id":"IJngTH4BwNWUzoUlgfZc"
+        }  
+    }
+    
+    }
+
+
     '''
 
-    def initialworkdays(self):
-        workdays = self.searchAll('workday/_search?size=2000&q=workday:true')
-        workdays = list( datetime.datetime.strptime(x['date'],'%Y%m%d').date() for x in workdays)
-        workrange=[]
-        for workday in workdays:
-            wd = str(workday)
-            wd = parse(wd)
-            workrange.append(
-                {
-                    'date':str(workday)
-                    ,'start':wd+datetime.timedelta(hours=9)
-                    ,'end':wd+datetime.timedelta(hours=12)
-                }
-            )
-            workrange.append(
-                {
-                    'date':str(workday)
-                    ,'start':wd+datetime.timedelta(hours=13)
-                    ,'end':wd+datetime.timedelta(hours=17.5)
-                }
-            )
-        self.workdays=workdays
-        self.workrange=workrange
-
-    def __init__(self,host,auth):
+    def __init__(self,host,auth=("username","password")):
         '''
+        host:服务器地址及端口，格式："http://xxxxxxx:9200"
         auth:账号信息，格式： ('username','password')
-        '''        
+        '''
         self.host=host
         self.auth=auth
         self.headers= {'Content-Type': 'application/json'}
 
-    def getworkhours(self,start,end):
-        workdays=self.workdays
-        #简易的时间合规函数,碰到时区不同只能处理utc时间和北京时间
-        def reguladate(dt):
-            if dt.tzname() == 'UTC':
-                timestr=datetime.datetime.strftime(dt,'%Y-%m-%dT%H:%M:%S+00:00')
-            else:
-                timestr=datetime.datetime.strftime(dt,'%Y-%m-%dT%H:%M:%S+08:00')
-            dt = parse(timestr)
-            return dt
-        #核心算法,计算同一天内的工作小时数
-        def getcdhours(start,end):   
-            final = 0.0
-            tday = list(filter(lambda x:x['date']==str(start.date()),self.workrange))
-            if not tday==[]: 
-                '''核心算法,实际上是取两条线段相交的部分'''   
-                for i in tday:
-                    ts=reguladate(i['start'])
-                    te=reguladate(i['end'])
-                    a1=start<ts
-                    a2=start>=ts and start <= te
-                    a3=start>te #开始时间大于范围结束,不用考虑
-                    b1= end<ts  #结束时间大于范围开始,不用考虑
-                    b2=end>=ts and end <=te
-                    b3= end>te
-                    if a3 or b1:
-                        continue
-                    if a1 and b2:
-                        th= (end-ts).total_seconds()
-                    if a1 and b3:
-                        th=  (te-ts).total_seconds()
-                    if a2 and b2:
-                        th=  (end-start).total_seconds()
-                    if a2 and b3:
-                        th=  (te-start).total_seconds()   
-                    final+=th
-            return final/3600
-        #时间合规
-        start = reguladate(start)
-        end = reguladate(end)
-        totalhours=0.0
-        #在当天就直接出结果
-        if start.date()==end.date():      
-            if start.date() in workdays:  
-                return getcdhours(start,end)
-        else:
-            '''
-            开始和结束不在同一天：
-            1.首先计算第一天的工作小时数，把开始时间赋值给临时变量后自增一天   
-            2.while循环:判断临时变量和结束日期是否在同一天，不在就直接加7.5个小时，得判断是不是工作日,然后再自增一天
-            3.等到在同一天了，把时间改为9点，再使用临时变量和结束时间进行计算
-            '''
-            tmp=datetime.datetime(start.year,start.month,start.day,17,30)
-            tmp=reguladate(tmp)
-            totalhours+=getcdhours(start,tmp)
-            tmp=start+datetime.timedelta(days=1)
-            '''如果临时日期仍然小于结束日期，且属于工作日，则直接加7.5工作小时'''  
-            while(tmp.date()<end.date()):          
-                if tmp.date() in workdays:
-                    totalhours+=7.5
-                tmp+=datetime.timedelta(days=1)   
-            tmp=datetime.datetime(tmp.year,tmp.month,tmp.day,9,0)   
-            tmp=reguladate(tmp) 
-            totalhours+=getcdhours(tmp,end)
-        return totalhours
+    #删除特定文档，需要索引和_id
     def delete(self,index,id):
         url = '{}/{}/_doc/{}'.format(self.host,index,id)
-        req = requests.delete(url,auth=self.auth)        
+        req = requests.delete(url,auth=self.auth,verify=False)        
         return json.loads(req.text)
-        #日期时间格式处理
+
+    #日期时间格式处理
     def Strfelktime(self,dt):
         '''
         将字符串格式的时间日期转换成elk能够识别的格式,含北京时区
         '''
+        if type(dt)==datetime.date:
+            timestr=datetime.date.strftime(dt,'%Y-%m-%dT%H:%M+08:00')
+            return timestr
         if type(dt)==str:
-            dt=parse(dt)
+            dt=parse(dt)        
         if dt.tzname() == 'UTC':
             timestr=datetime.datetime.strftime(dt,'%Y-%m-%dT%H:%M+00:00')
         else:
             timestr=datetime.datetime.strftime(dt,'%Y-%m-%dT%H:%M+08:00')
         return timestr
+    #单条数据录入，body中需要有id字段，将自动作为_id处理
     def put(self,index,obj):
         '''
         录入elk的方法
@@ -131,11 +81,10 @@ class elkhelper():
         '''
         data = json.dumps(obj,ensure_ascii=True)
         url='{}/{}/_doc/{}'.format(self.host,index,obj['id'])        
-        req=requests.put(url,auth=self.auth,headers=self.headers,data=data.encode())
+        req=requests.put(url,auth=self.auth,headers=self.headers,data=data.encode(),verify=False)
         result = json.loads(req.text)
         return result
-
-        
+    #批量录入数据到es，目前仅支持同一个索引
     def bulk(self,index,dl):
         reqtext=''
         for obj in dl:
@@ -145,27 +94,15 @@ class elkhelper():
             d2=json.dumps(o2)
             d3=d1+'\n'+d2+'\n'
             reqtext+=d3
-        ss=requests.post(self.host+'/_bulk',auth=self.auth,headers=self.headers,data=reqtext.encode())
-        return ss.text
-    def putDictList(self,index,dl):
-        '''字典列表直接入elk
-        自动将日期时间转换为elk时间
-        同样每个字典需要id字段
-        '''
-        for d in dl:
-            for f in d:
-                if type(d[f])==datetime.datetime:
-                    d[f]=self.Strfelktime(d[f])
-                if type(d[f])==datetime.date:
-                    d[f]=str(d[f])
-            self.put(index,d)
-    
+        ss=requests.post(self.host+'/_bulk',auth=self.auth,headers=self.headers,data=reqtext.encode(),verify=False)  
+        return json.loads(ss.text)
+    #根据索引和ID获取单条数据
     def getdoc(self,index,id):
         url = '{}/{}/_doc/{}'.format(self.host,index,id)
         req = requests.get(url,auth=self.auth)
         res = json.loads(req.text)
         return res
-
+    #单页查询,最大支持10000条数据
     def searchdoc(self,querystr):
         '''
         单页查询,最大支持10000条数据
@@ -173,29 +110,48 @@ class elkhelper():
         index/_search?size=xxxx?q=xxxx:xxx                        
         '''
         url = '{}/{}'.format(self.host,querystr)
-        req = requests.get(url,auth=self.auth)
+        req = requests.get(url,auth=self.auth,verify=False)
         res =json.loads(req.text)
         if not 'error' in res:
             result = list(x for x in res['hits']['hits'])
             return result
         else:
             return []
-        
-    def search(self,querystr):
-        '''
-        单页查询,最大支持10000条数据
-        querystr直接写索引名加搜索字符串,如下
-        index/_search?size=xxxx?q=xxxx:xxx                        
-        '''
-        url = '{}/{}'.format(self.host,querystr)
-        req = requests.get(url,auth=self.auth)
-        res =json.loads(req.text)
-        if not 'error' in res:
-            result = list(x['_source'] for x in res['hits']['hits'])
-            return result
-        else:
-            return []
-        
+    #以当前时间获取kibana支持的时间戳字符串
+    def get_es_stamp(self):
+        return datetime.datetime.strftime(datetime.datetime.now(),'%Y-%m-%dT%H:%M+08:00')
+    #单字段匹配查询
+    def query(self,index,field,str,size=8000):
+        body={
+        "query":{
+            "wildcard":{field+".keyword": {"value":"*{}*".format(str)}}
+        }
+        }
+        url = '{}/{}/_search?size={}'.format(self.host,index,size)
+        data = json.dumps(body,ensure_ascii=True)
+        res = requests.post(url,headers=self.headers,auth=self.auth,data=data.encode(),verify=False)
+        result = json.loads(res.text)
+        data = result['hits']['hits']
+        return data
+    #索引删除
+    def idxDel(self,idx):
+        url = '{}/{}'.format(self.host,idx)
+        res = requests.delete(url,headers=self.headers,auth=self.auth,verify=False)
+        result = json.loads(res.text)
+        return result
+    #单字段匹配删除
+    def deletebyquery(self,index,field,str):
+        body={
+            "query":{
+                "wildcard":{field+".keyword": {"value":"*{}*".format(str)}}
+            }
+        }
+        url = '{}/{}/_delete_by_query'.format(self.host,index)
+        data = json.dumps(body,ensure_ascii=True)
+        res = requests.post(url,headers=self.headers,auth=self.auth,data=data.encode())
+        result = json.loads(res.text)
+        return result
+    #翻页获取数据
     def searchAll(self,querystr):
         '''
         全量数据查询,当所需要的数据量大于10000条时使用
@@ -203,7 +159,7 @@ class elkhelper():
         index/_search?size=xxxx?q=xxxx:xxx   
         '''
         url = '{}/{}&scroll=1m'.format(self.host,querystr)
-        req = requests.get(url,auth=self.auth)
+        req = requests.get(url,auth=self.auth,verify=False)
         result =json.loads(req.text)
         if 'hits' in result:
             data = list(x['_source'] for x in result['hits']['hits'])
@@ -214,8 +170,16 @@ class elkhelper():
         total = result['hits']['total']['value']
         while len(data)<total:
             url='{}/_search/scroll?scroll_id={}&scroll=1m'.format(self.host,sid)
-            req=requests.get(url,auth=self.auth)
+            req=requests.get(url,auth=self.auth,verify=False)
             result=json.loads(req.text)
             data+=list(x['_source'] for x in result['hits']['hits'])
         return data
-
+    #巨量数据时翻页批量录入，方法同bulk
+    def bulkbypage(self,data,limit):
+        page = int(len(data)/limit)
+        if len(data)%limit !=0:
+            page+=1
+        for i in range(0,page):
+            req = data[i*limit:][:limit]
+            res = json.loads(self.bulk('sh-crm-incomes',req))
+            print(res['errors'])
--- practice.py
+++ practice.py
@@ -1,81 +1,41 @@
+from pydoc import importfile
 from greg_pkg_release.elkhelper import elkhelper
+import random
+
+elk = elkhelper("http://121.36.91.101:19200",("testman","testman"))
 
 
-# elk = elkhelper('http://yourserver:19200',('username','password'))
-elk=elkhelper()
+ln=[
+"赵","钱","孙","李",
+"冯","陈","褚","卫",
+"朱","秦","尤","许",
+"孔","曹","严","华",
+"戚","谢","邹","喻",
+"云","苏","潘","葛",
+"鲁","韦","昌","马",
+"俞","任","袁","柳"
+]
 
-table=[]
-workday1 ={
-    'id':1,'date':'2021-06-07'
-}
-workday2 ={
-    'id':2,'date':'2021-06-08'
-}
-table.append(workday1)
-table.append(workday2)
+fn=[
+    "木玮","超润","卓昊","梓骆","记铭","林木","月玮","远宏","伯峻","伦强","谷韶","彬帆","玮云","天霖","斯辰","寒月","璟良","志嘉","天瑞"
+]
+age =range(16,19)
 
+bn = "A0201"
+total=100
+nameused=[]
+students=[]
+for i in range(1,50):
+    name =f"{random.choice(ln)}{random.choice(fn)}"
+    while name in nameused:
+        name =f"{random.choice(ln)}{random.choice(fn)}"
+    obj={
+        "id":bn+str(i).zfill(3),
+        "age":random.choice(age),
+        "name":name
+    }
+    students.append(obj)
 
-index='workdaytest'
-# res = elk.searchAll('{}/_search?size=1'.format(index))
+res = elk.bulk("test_students",students)
 
-
-workday3 ={
-    'id':3,'date':'2021-06-09'
-}
-
-res = elk.put(index,workday3)
-
-ss=0
-
-
-
-# res =elk.bulk(index,table)
-# print(res)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# res = elk.searchAll('workday/_search?size=2000')
-
-# workdays =[]
-
-# workday = {
-#     'id':1
-#     ,'time':'2021-06-06'
-# }
-
-# workdays.append(workday)
-
-# workday2= {
-#     'id':4
-#     ,'time':'2021-06-17'
-# }
-
-# workdays.append(workday2)
-# # res = elk.put('workdaytest',workday)
-
-# res = elk.bulk('workdaytest',workdays)
-
-
-# print(result)
-# # res = elk.searchAll('workdaytest/_search?size=2000')
-
-
-# ss=0
-
+print(res)
